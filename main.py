955046c5e0f377b1bbb0e6e3b8f711282c895530
HEMA <hemayalavarthi1631@gmail.com>
2026-06-28 20:01:02 +0530

    3rd video

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 app=FastAPI()
 app.mount('/static',StaticFiles(directory="static"),name="static")
 #Telling FastAPI where to look for templates
@@ -56,6 +57,22 @@ def post_page(request: Request, post_id: int):
 def get_posts():
     return posts
 
+app.post(
+    "/api/posts",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025",
+    }
+    posts.append(new_post)
+    return new_post
 
 @app.get("/api/posts/{post_id}")
 def get_post(post_id: int):
